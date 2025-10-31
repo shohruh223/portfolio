6c6d599fca2c0423889407e3294e2fb43a1ce130
Shohruh223 <shohruh.abd2223@gmail.com>
2025-10-31 11:20:53 +0500

    add new change 3

--- app/main.py
+++ app/main.py
@@ -2,16 +2,10 @@ from fastapi import FastAPI, Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
 from starlette.staticfiles import StaticFiles
-from app.model import Base, engine
 
 app = FastAPI()
 templates = Jinja2Templates(directory="app/templates")
 app.mount("/static", StaticFiles(directory='app/static'), name="static")
-
-
-@app.on_event("startup")
-def on_startup():
-    Base.metadata.create_all(bind=engine)
 
 
 @app.get("/", response_class=HTMLResponse)
--- app/model.py
+++ app/model.py
@@ -1,16 +1,16 @@
-# app/model.py
-import os
-from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
-
-DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:////home/PortfolioMe/portfolio/data.db"
-
-# SQLite bo'lsa, connect_args kerak bo'ladi
-connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
-
-engine = create_engine(DATABASE_URL, pool_pre_ping=True, connect_args=connect_args)
-SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
-Base = declarative_base()
-
-# ⚠️ MUHIM: BU YERDAN create_all() ni olib tashlang!
-# Base.metadata.create_all(engine)  # <-- O'CHIRING
+# # app/model.py
+# import os
+# from sqlalchemy import create_engine
+# from sqlalchemy.orm import sessionmaker, declarative_base
+#
+# DATABASE_URL = os.getenv("DATABASE_URL") or "sqlite:////home/PortfolioMe/portfolio/data.db"
+#
+# # SQLite bo'lsa, connect_args kerak bo'ladi
+# connect_args = {"check_same_thread": False} if DATABASE_URL.startswith("sqlite") else {}
+#
+# engine = create_engine(DATABASE_URL, pool_pre_ping=True, connect_args=connect_args)
+# SessionLocal = sessionmaker(bind=engine, autocommit=False, autoflush=False)
+# Base = declarative_base()
+#
+# # ⚠️ MUHIM: BU YERDAN create_all() ni olib tashlang!
+# # Base.metadata.create_all(engine)  # <-- O'CHIRING
